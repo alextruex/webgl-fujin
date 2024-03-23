3022877f7f09db6b85958a022c42f350fa5adffa
Alex Truex <ahtruex@gmail.com>
2024-03-23 15:29:50 -0400

    add fallbacks for resource loading

--- src/video/texturecache.ts
+++ src/video/texturecache.ts
@@ -1,53 +1,49 @@
-const TEX = [
-    '00_test.png',
-    '01_player.png',
-    '02_enemies.png'
-]
-
+const TEX_COUNT = 2;
 const TEX_SIZE = 256;
 
 class TextureCache {
     textures: Array<WebGLTexture>;
     txSize: number;
-    txIn: number;
-    txOut: number;
+    in: number;
+    out: number;
 
     constructor(gl: WebGLRenderingContext) {
         this.textures = [];
         this.txSize = TEX_SIZE;
-        this.txIn = TEX.length;
-        this.txOut = 0;
+        this.in = TEX_COUNT;
+        this.out = 0;
 
         // Fallback
-        for (let i = 0; i < TEX.length; i++) {
-            this.textures[i] = <WebGLTexture>gl.createTexture();
-            gl.bindTexture(gl.TEXTURE_2D, this.textures[i])
-            let pixels:Array<number> = [];
-            let color = 0;
-            for(let i = 0; i < 256; i+=3){
-                pixels.push(240);
-                pixels.push(128);
-                pixels.push(32);
-            }
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, 16, 16, 0,
-            gl.LUMINANCE, gl.UNSIGNED_BYTE, new Uint8Array(pixels));
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-            
+        let fallback = <WebGLTexture>gl.createTexture();
+        let pixels:Array<number> = [];
+        for(let i = 0; i < 256; i+=3){
+            pixels.push(240);
+            pixels.push(128);
+            pixels.push(32);
         }
-        
-        for (let i = 0; i < TEX.length; i++) {
+        gl.bindTexture(gl.TEXTURE_2D, fallback)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, 16, 16, 0,
+            gl.LUMINANCE, gl.UNSIGNED_BYTE, new Uint8Array(pixels));
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+        for (let i = 0; i < TEX_COUNT+1; i++) {
+            this.textures[i] = fallback;            
+        }
+
+        // Load textures
+        for (let i = 1; i < TEX_COUNT+1; i++) {
             let image = new Image();
-            image.src = 'tex/' + TEX[i];
+            image.src = 'tex/tex_' + i + '.png';
             image.addEventListener('load', () => {
+                this.textures[i] = <WebGLTexture>gl.createTexture();
                 gl.bindTexture(gl.TEXTURE_2D, this.textures[i]);
                 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
                 gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
                 gl.generateMipmap(gl.TEXTURE_2D);
-                this.txOut++;
+                this.out++;
             });
         }
     }
--- src/video/vertexcache.ts
+++ src/video/vertexcache.ts
@@ -1,95 +1,100 @@
-const MDL = [
-    '00_sprite.obj',
-    '01_cube.obj',
-    '02_torus.obj',
-    '03_icosahedron.obj',
-    '04_monkey.obj'
-];
+const MODEL_COUNT = 4;
 
 class VertexCache{
     buffer:WebGLBuffer;
     verts:Array<Array<number>>;
     start:Array<number>;
     count:Array<number>;
-    mdIn:number;
-    mdOut:number;
+    in:number;
+    out:number;
 
     constructor(gl:WebGLRenderingContext){
         this.buffer = <WebGLBuffer>gl.createBuffer();
         this.verts = [];
         this.start = [];
         this.count = [];
-        this.mdIn = MDL.length;
-        this.mdOut = 0;
+        this.in = MODEL_COUNT;
+        this.out = 0;
 
         // Fallback
-        for(let i = 0; i < MDL.length; i++){
-            this.verts.push([
-                32.0,-32.0,0.0,1.0,0.0,
-                -32.0,32.0,0.0,0.0,1.0,
-                -32.0,-32.0,0.0,0.0,0.0,
-                32.0,-32.0,0.0,1.0,0.0,
-                32.0,32.0,0.0,1.0,1.0,
-                -32.0,32.0,0.0,0.0,1.0,
-                ]);
-            this.start.push(0);
+        this.verts[0] = [
+            32.0,-32.0,0.0,1.0,0.0,
+            -32.0,32.0,0.0,0.0,1.0,
+            -32.0,-32.0,0.0,0.0,0.0,
+            32.0,-32.0,0.0,1.0,0.0,
+            32.0,32.0,0.0,1.0,1.0,
+            -32.0,32.0,0.0,0.0,1.0
+        ]
+        for(let i = 0; i < MODEL_COUNT+1; i++){
+            this.start[i] = 0;
+            this.count[i] = 6;
         }
-        this.compileBuffer(gl);
+        gl.bindBuffer(gl.ARRAY_BUFFER,this.buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.verts[0]), gl.STATIC_DRAW);
     
         // Load Textures
-        for(let i = 0; i < MDL.length; i++){
+        for(let i = 1; i < MODEL_COUNT+1; i++){
             let req = new XMLHttpRequest();
-            req.open('GET','mdl/' + MDL[i]);
+            req.open('GET','mdl/mdl_' + i + '.obj');
             req.send();
             req.onreadystatechange = (e) => {
                 if(req.readyState == 4){
-                    let data:Array<number> = [];
-                    let verts:Array<Array<number>> = [];
-                    let uv:Array<Array<number>> = [];
-                    let faces:Array<Array<string>> = [];
-        
-                    let rows = req.responseText.split('\n');
-                    for(let r in rows){
-                        let row = rows[r].split(' ');
-                        if (row[0] == 'v') verts.push([+row[1],+row[2],+row[3]]);
-                        if (row[0] == 'vt') uv.push([+row[1],+row[2]]);
-                        if (row[0] == 'f') faces.push([row[1],row[2],row[3]]);
+                    let data = [0];
+                    try{
+                        data = this.parseObj(req.responseText);
                     }
-        
-                    for(let f in faces){ 
-                        let face = faces[f]; // Each face
-                        for(let vrt in face){
-                            let vertex = face[vrt].split('/'); // Each vertex
-                            let pos = +vertex[0]-1;
-                            let tex = +vertex[1]-1;
-                            data.push(verts[pos][0]);
-                            data.push(verts[pos][1]);
-                            data.push(verts[pos][2]);
-                            data.push(uv[tex][0]);
-                            data.push(uv[tex][1]);
-                        }
+                    catch{
+                        data = this.verts[0];
+                        console.log('Error parsing model');
                     }
-                    
                     this.verts[i] = data;
-                    this.mdOut++;
-
-                    if(this.mdIn == this.mdOut){
+                    this.out++;
+                    if(this.in == this.out){
                         this.compileBuffer(gl);
                     }
                 }
             }
         }
     }
-    
 
+    parseObj(text:string){
+        let data:Array<number> = [];
+        let verts:Array<Array<number>> = [];
+        let uv:Array<Array<number>> = [];
+        let faces:Array<Array<string>> = [];
+
+        let rows = text.split('\n');
+        for(let r in rows){
+            let row = rows[r].split(' ');
+            if (row[0] == 'v') verts.push([+row[1],+row[2],+row[3]]);
+            if (row[0] == 'vt') uv.push([+row[1],+row[2]]);
+            if (row[0] == 'f') faces.push([row[1],row[2],row[3]]);
+        }
+
+        for(let f in faces){ 
+            let face = faces[f]; // Each face
+            for(let vrt in face){
+                let vertex = face[vrt].split('/'); // Each vertex
+                let pos = +vertex[0]-1;
+                let tex = +vertex[1]-1;
+                data.push(verts[pos][0]);
+                data.push(verts[pos][1]);
+                data.push(verts[pos][2]);
+                data.push(uv[tex][0]);
+                data.push(uv[tex][1]);
+            }
+        }
+
+        return data;
+    }
+    
     compileBuffer(gl:WebGLRenderingContext){
         let data:Array<number> = [];
-        for(let i = 0; i < MDL.length; i++){
+        for(let i = 0; i < MODEL_COUNT+1; i++){
             this.start[i] = data.length / 5;
             data = data.concat(this.verts[i]);
             this.count[i] = this.verts[i].length / 5;
         }
-        console.log(data.length);
         gl.bindBuffer(gl.ARRAY_BUFFER,this.buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
     }
